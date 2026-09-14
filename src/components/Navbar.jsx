e4a578229bc32df35b17104d2cbaabe50b9158db
Lashy <hfirman505@gmail.com>
2026-09-14 07:26:31 +0700

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,37 +4,60 @@ import logoIf from "../assets/ti-logo.png";
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="brand">
-        {/* Logo UMP di sebelah kiri */}
-        <img src={logoUmp} alt="Logo UMP" className="logo-ump" />
-        <div className="ump-text">
-         <strong>Universitas</strong>
 
-          <span className="tagline">Muhammadiyah</span>
+      <div className="brand">
+
+        {/* LOGO UMP */}
+        <img
+          src={logoUmp}
+          alt="Logo UMP"
+          className="logo-ump"
+        />
+
+        {/* TEXT UMP */}
+        <div className="ump-text">
+          <strong>Universitas</strong>
+
+          <span className="ump-muhammadiyah">
+            Muhammadiyah
+          </span>
 
           <small>Purwokerto</small>
         </div>
 
+        {/* GARIS PEMISAH */}
         <div className="logo-divider"></div>
 
-        {/* Logo IF di sebelah kanan */}
-        <img src={logoIf} alt="Logo Teknik Informatika" className="logo-if" />
+        {/* LOGO TEKNIK INFORMATIKA */}
+        <img
+          src={logoIf}
+          alt="Logo Teknik Informatika"
+          className="logo-if"
+        />
 
+        {/* TEXT TEKNIK INFORMATIKA */}
         <div className="brand-text">
           <strong>Teknik Informatika</strong>
 
-          <span className="tagline">smart • creative • progressive</span>
+          <span className="if-tagline">
+            smart • creative • progressive
+          </span>
 
-          <small>Universitas Muhammadiyah Purwokerto</small>
+          <small>
+            Universitas Muhammadiyah Purwokerto
+          </small>
         </div>
+
       </div>
 
+      {/* NAVIGATION */}
       <div className="nav-links">
         <a href="#home">Home</a>
         <a href="#about">About</a>
         <a href="#events">Events</a>
         <a href="#contact">Contact</a>
       </div>
+
     </nav>
   );
 }
