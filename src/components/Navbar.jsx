c64572bab0ecc24a42bb58a1a544819652864062
Lashy <hfirman505@gmail.com>
2026-09-14 07:16:53 +0700

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,42 @@
 import logoUmp from "../assets/logo-ump.png";
-import logoIf from "../assets/ti-logo.png"; 
+import logoIf from "../assets/ti-logo.png";
 
-function Navbar(){
-    return(
+function Navbar() {
+  return (
     <nav className="navbar">
-        <div className="brand">
-          {/* Logo UMP di sebelah kiri */}
-          <img
-            src={logoUmp}
-            alt="Logo UMP"
-            className="logo-ump"
-          />
+      <div className="brand">
+        {/* Logo UMP di sebelah kiri */}
+        <img src={logoUmp} alt="Logo UMP" className="logo-ump" />
+        <div className="ump-text">
+         <strong>Universitas</strong>
 
-          {/* Logo IF di sebelah kanan */}
-          <img
-            src={logoIf}
-            alt="Logo Teknik Informatika"
-            className="logo-if"
-          />
+          <span className="tagline">Muhammadiyah</span>
 
-          <div className="brand-text">
-            <strong>Teknik Informatika</strong>
-
-            <span className="tagline">
-              smart • creative • progressive
-            </span>
-
-            <small>
-              Universitas Muhammadiyah Purwokerto
-            </small>
-          </div>
+          <small>Purwokerto</small>
         </div>
 
-        <div className="nav-links">
-          <a href="#home">Home</a>
-          <a href="#about">About</a>
-          <a href="#events">Events</a>
-          <a href="#contact">Contact</a>
+        <div className="logo-divider"></div>
+
+        {/* Logo IF di sebelah kanan */}
+        <img src={logoIf} alt="Logo Teknik Informatika" className="logo-if" />
+
+        <div className="brand-text">
+          <strong>Teknik Informatika</strong>
+
+          <span className="tagline">smart • creative • progressive</span>
+
+          <small>Universitas Muhammadiyah Purwokerto</small>
         </div>
-      </nav> 
-    );
+      </div>
+
+      <div className="nav-links">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#events">Events</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
